5948da14a7f16f5bed965f150576f0568bd97a50
Curtis Fenner <3289892+CurtisFenner@users.noreply.github.com>
2026-09-10 13:30:03 +0900

    Add react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 // @ts-check
+import { unified } from "@astrojs/markdown-remark";
 import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
@@ -8,15 +10,21 @@ import remarkMath from "remark-math";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://curtisfenner.com",
-	integrations: [mdx(), sitemap()],
+	integrations: [
+		react(),
+		mdx(),
+		sitemap(),
+	],
 
 	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [() => rehypeKatex({
-			trust: (context) => {
-				return context.command === "\\htmlClass";
-			},
-		})],
+		processor: unified({
+			remarkPlugins: [remarkMath],
+			rehypePlugins: [() => rehypeKatex({
+				trust: (context) => {
+					return context.command === "\\htmlClass";
+				},
+			})],
+		}),
 	},
 	devToolbar: {
 		enabled: false
